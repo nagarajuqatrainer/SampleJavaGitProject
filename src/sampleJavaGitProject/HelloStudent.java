4f83244b48ad339a97f30b293aff7dda05f9fc17
nagaraju <nagaraju.qatrainer@gmail.com>
2025-08-28 15:32:50 +0530

    New feature added

--- src/sampleJavaGitProject/HelloStudent.java
+++ src/sampleJavaGitProject/HelloStudent.java
@@ -5,6 +5,13 @@ public class HelloStudent {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Student");
+		System.out.println("Hello mastaru");
+		System.out.println("Hello Student");
+		System.out.println("Hello mastaru");
+		System.out.println("Hello Student");
+		System.out.println("Hello mastaru");
+		System.out.println("Hello Student");
+		System.out.println("Hello mastaru");
 	}
 
 }
